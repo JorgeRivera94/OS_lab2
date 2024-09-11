31edbcd6393938017a9a41ee3a111a59984d6877
JorgeRivera94 <jrivera28901@gmail.com>
2024-09-11 17:39:27 -0400

    Now complies with first bullet

--- Exercise_1/src/greetings.c
+++ Exercise_1/src/greetings.c
@@ -4,8 +4,18 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <wait.h>
 
 void Greet() {
+  // process ID
   pid_t pid;
+
+  // print message
   printf("hello there, I’m process %d\n", getpid());
+
+  // wait
+  sleep(10);
+
+  // print second message
+  printf("I’m done, bye!\n");
 }
