832dbc44585f34e969c9361361364f55275e0fcb
JorgeRivera94 <jrivera28901@gmail.com>
2024-09-11 18:47:43 -0400

    can create children and inner children

--- Exercise_1/src/greetings.c
+++ Exercise_1/src/greetings.c
@@ -13,9 +13,32 @@ void Greet() {
   // print message
   printf("hello there, I’m process %d\n", getpid());
 
-  // wait
+  // wait 10s
   sleep(10);
 
   // print second message
   printf("I’m done, bye!\n");
+}
+
+// here the progam that creates 3 children
+void CreateChildren() {
+  pid_t id;
+  pid_t cid;
+
+  for (int i = 0; i < 3; i++) {
+    id = fork();  // 0 para children
+    if (id == 0) {
+      printf("I’m a child, and my id is: %d\n", getpid());
+      for (int j = 0; j < 2; j++) {
+        cid = fork();
+        if (cid == 0) {
+          printf("inner child %d\n", getpid());
+          exit(0);
+        }
+      }
+      exit(0);
+    }
+  }
+
+  printf("This is the parent pid: %d\n", getpid());
 }
--- Exercise_1/src/main.c
+++ Exercise_1/src/main.c
@@ -1,4 +1,5 @@
 int main() {
   Greet();
+  CreateChildren();
   return 0;
 }
