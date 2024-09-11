e1f347125a904812464dad50db98c8552aabba55
JorgeRivera94 <jrivera28901@gmail.com>
2024-09-11 18:56:46 -0400

    copied greetings to its own project

---
+++ greeting/src/greetings.c
@@ -0,0 +1,20 @@
+#include "greetings.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void Greet() {
+  // process ID
+  pid_t pid;
+
+  // print message
+  printf("hello there, I’m process %d\n", getpid());
+
+  // wait 10s
+  sleep(10);
+
+  // print second message
+  printf("I’m done, bye!\n");
+}
---
+++ greeting/src/main.c
@@ -0,0 +1,5 @@
+int main() {
+  Greet();
+
+  return 0;
+}
