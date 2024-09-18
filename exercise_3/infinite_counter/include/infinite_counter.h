dda4858cb35cf0b847e81ff51a1a3418cec501d3
JorgeRivera94 <jrivera28901@gmail.com>
2024-09-17 21:15:28 -0400

    documented infinite_counter

--- exercise_3/infinite_counter/include/infinite_counter.h
+++ exercise_3/infinite_counter/include/infinite_counter.h
@@ -1,7 +1,23 @@
 #ifndef INFINITE_COUNTER_H
 #define INFINITE_COUNTER_H
 
+/**
+ * @brief Signal handler for the infinite counter.
+ *
+ * Resets the counter to 0 if SIGUSR1 signal is received. Changes counting
+ * direction if SIGUSR2 signal is received.
+ * @param signal Signal to be received by counter_handler
+ */
 void Handler(int signal);
+
+/**
+ * @brief A counter function that counts infinitely.
+ *
+ * Start counting in the positive direction from 0. Direction can be changed by
+ * the Handler() function. Handler() function can also reset the counter to 0.
+ * This funtion advances the counter in the current direction, then sleeps for
+ * one second.
+ */
 void Count();
 
 #endif
